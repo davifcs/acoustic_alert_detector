fbfb2c82d5bd52c058ef05d08cb7ffa0f0878bda
Davi Santos <davi.fcs@gmail.com>
2022-06-19 13:03:31 -0300

    Added GhostNet to export.py

--- export.py
+++ export.py
@@ -5,6 +5,7 @@ import torch.onnx
 import onnxruntime as ort
 import numpy as np
 from models.convolutional import DSCNN, CNN2D, CNN1D
+from models.deepwise import GhostNet
 
 
 argparser = argparse.ArgumentParser(description='Export to ONNX format')
@@ -15,7 +16,7 @@ argparser.add_argument('-m',
 
 argparser.add_argument('-mt',
                        '--model_type',
-                       help='Select among: CNN1D, CNN2D, DSCNN, TRANS')
+                       help='Select among: CNN1D, CNN2D, DSCNN, GN')
 
 
 argparser.add_argument('-i',
@@ -32,6 +33,35 @@ def convert_onnx(model_path, model_type, input_size):
         model = CNN2D().load_from_checkpoint(checkpoint_path=model_path)
     elif model_type == "CNN1D":
         model = CNN1D().load_from_checkpoint(checkpoint_path=model_path)
+    elif model_type == "GN":
+        cfgs = [
+            # k, t, c, SE, s
+            # stage1
+            [[3, 16, 16, 0, 1]],
+            # stage2
+            [[3, 48, 24, 0, 2]],
+            [[3, 72, 24, 0, 1]],
+            # stage3
+            [[5, 72, 40, 0.25, 2]],
+            [[5, 120, 40, 0.25, 1]],
+            # stage4
+            [[3, 240, 80, 0, 2]],
+            [[3, 200, 80, 0, 1],
+             [3, 184, 80, 0, 1],
+             [3, 184, 80, 0, 1],
+             [3, 480, 112, 0.25, 1],
+             [3, 672, 112, 0.25, 1]
+             ],
+            # stage5
+            [[5, 672, 160, 0.25, 2]],
+            [[5, 960, 160, 0, 1],
+             [5, 960, 160, 0.25, 1],
+             [5, 960, 160, 0, 1],
+             [5, 960, 160, 0.25, 1]
+             ]
+        ]
+        model = GhostNet.load_from_checkpoint(checkpoint_path=model_path,
+                                              cfgs=cfgs, width=0.2, learning_rate=0.01, log_path='./', patience=20)
 
     model.eval()
     dummy_input = torch.randn(input_size, requires_grad=True)
